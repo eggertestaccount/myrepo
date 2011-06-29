b8d325d4e2cfef321263c28a8f35c67bbdd2fc69
Adam Charnock <adam@omniwiki.co.uk>
2011-06-29 18:47:38 +0100

    Adding cache busting (mtime) value to the end of URLs

--- src/djjsmin/templatetags/jsmin.py
+++ src/djjsmin/templatetags/jsmin.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import os
 import os.path
 
 from django import template
@@ -19,10 +20,18 @@ class JavascriptsNode(template.Node):
         root = utils.get_root(settings)
         input_files, temp_files = utils.resolve_patterns(settings.JSMIN_INPUT, root)
         
-        make_relative = lambda f: os.path.relpath(f, settings.MEDIA_ROOT)
-        input_files = map(make_relative, input_files)
-        temp_files = map(make_relative, temp_files)
+        def urlize(f):
+            path = os.path.relpath(f, settings.MEDIA_ROOT)
+            try:
+                path += "?_=%d" % os.stat(f).st_mtime
+            except OSError:
+                pass
+            
+            return path
         
+        input_files = map(urlize, input_files)
+        temp_files = map(urlize, temp_files)
+
         html = ""
         for f in input_files + temp_files:
             html += """<script src="%s%s" type="text/javascript" charset="utf-8"></script>\n""" % (settings.MEDIA_URL, f)
